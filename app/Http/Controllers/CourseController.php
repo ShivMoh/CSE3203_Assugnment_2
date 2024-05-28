8876f40bec90957f407091f4f16eb5d3c5691b3b
shivmoh <shiveshmohamed1234@gmail.com>
2024-05-27 21:59:34 -0400

    fixed something

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -9,7 +9,8 @@ use App\Models\Course;
 class CourseController extends Controller
 {
     public function get_all_courses() {
-        $courses = Course::orderBy('id', 'asc')->get();
+        $user_id = "101754e0-1748-4c33-8fe6-94f9c64babdb";
+        $courses = Course::where("user_id", $user_id)->orderBy('id', 'asc')->get();
         return view('courses', [
             'courses' => $courses
         ]);
